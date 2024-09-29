d484a057f13b5867042d0b1353a0a52a597e3fd0
Abhishek Yadav <148934908+Abhidev-aiml@users.noreply.github.com>
2024-09-29 21:49:40 +0530

    final without skills'

--- portfolio/src/components/home/Social.jsx
+++ portfolio/src/components/home/Social.jsx
@@ -5,15 +5,15 @@ import { UilGithubAlt } from '@iconscout/react-unicons';
 const Social = () => {
   return (
 <div className="home__social">
-    <a href="https://www.instagram.com/" className='home__social-icon' target='_blank'>
-    <UilInstagram size="24" color="#000000"/>
+    <a href="https://www.instagram.com/lilabhilifts" className='home__social-icon' target='_blank'>
+    <UilInstagram size="26" color="#000000"/>
     </a>
     <a href="https://www.linkedin.com/in/abhidevs/" className='home__social-icon' target='_blank'>
-    <UilLinkedin size="24" color="#000000"/>
+    <UilLinkedin size="26" color="#000000"/>
  
     </a>
-    <a href="https://www.github.com/" className='home__social-icon' target='_blank'>
-    <UilGithubAlt size="24" color="#000000"/>
+    <a href="https://www.github.com/Abhidev-aiml" className='home__social-icon' target='_blank'>
+    <UilGithubAlt size="26" color="#000000"/>
 
     </a>
 </div>
